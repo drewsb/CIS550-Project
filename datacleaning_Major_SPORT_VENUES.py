022a9277e6cf3535c4d9e8613332f38872a85f67
Alex Matthys <alexmatthys@gmail.com>
2017-04-07 12:49:57 -0400

    separate datacleaning python scripts

---
+++ datacleaning_Major_SPORT_VENUES.py
@@ -0,0 +1,29 @@
+# script to take in a csv file and clean the data
+
+import csv
+
+#Script to clean Major_Sports_Venues.csv
+
+f = open('Major_Sport_Venues.csv', 'r', encoding='mac_roman')
+csv_f = csv.reader(f)
+
+stadiumAddress = set()
+primKey = set()
+
+cntr = 0
+for row in csv_f:
+	print(cntr)
+	cntr += 1
+	# Check for duplicate addresses
+	s = (row[2], row[3], row[5], row[6])
+	if s in stadiumAddress:
+		print("We have found a duplicate: " + str(s))
+	else:
+		stadiumAddress.add(s)
+	# Check for duplicate Primary Key's
+	if row[0] in primKey:
+		print("We have found a duplicate: " + str(row[0]))
+	else:
+		primKey.add(row[0])
+
+print("done")
